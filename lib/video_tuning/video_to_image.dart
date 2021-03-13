30bccfccd45146d527121158ddcda24fc5dc26f4
MatsumaruTsuyoshi <73928886+MatsumaruTsuyoshi@users.noreply.github.com>
2021-03-13 22:39:15 +0900

    コメント追加

--- lib/video_tuning/video_to_image.dart
+++ lib/video_tuning/video_to_image.dart
@@ -36,16 +36,21 @@ class VideoToImage extends ChangeNotifier {
       final FlutterFFprobe _flutterFFprobe = new FlutterFFprobe();
       int frameNum =
           await _flutterFFprobe.getMediaInformation("$file").then((info) {
+        //動画の時間を取得
         duration = info.getMediaProperties()['duration'];
         if (info.getAllProperties()['streams'][0]['r_frame_rate'] == '0/0') {
+          //FPSを取得
           realFrameRate = info.getAllProperties()['streams'][1]['r_frame_rate'];
         } else {
+          //FPSを取得
           realFrameRate = info.getAllProperties()['streams'][0]['r_frame_rate'];
         }
 
+        //分数を数値として扱えるようにする
         final fracRealFrameRate = Fraction.fromString(realFrameRate);
+        //取得した動画の時間は文字列なので変換する
         final durationDouble = double.parse(duration);
-
+        //動画をスプリットするフレーム数を計算する
         int frameNumber =
             (durationDouble * fracRealFrameRate.toDouble()).toInt();
         return frameNumber;
@@ -61,14 +66,17 @@ class VideoToImage extends ChangeNotifier {
       final FlutterFFmpeg _flutterFFmpeg = new FlutterFFmpeg();
       DateTime now = DateTime.now();
       String formattedDate = DateFormat('yyyy-MM-dd–kk-mm-ss').format(now);
+      //スプリットした画像を保存するパスを指定しておく
       String outputPath = "$path" "/${formattedDate}output%04d.jpg";
 
+      //ここで動画を指定したフレーム数に画像変換する
       await _flutterFFmpeg
           .execute("-i $file -vframes $frameNumber $outputPath")
           .then((rc) => print("FFmpeg process exited with rc $rc"));
 
       pathNameList = [];
 
+      //画像の保存先をリストに追加
       for (int i = 1; i < frameNumber + 1; i++) {
         String pathName =
             '$path/${formattedDate}output${i.toString().padLeft(4, "0")}.jpg';
